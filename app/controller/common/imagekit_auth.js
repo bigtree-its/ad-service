c6d80a67d4db877cf2507a6412ac26c6e4472974
bigtree_it_s <bigtree.it.services@gmail.com>
2024-05-24 16:53:03 +0100

    Add ImageKitToken endpoint

--- app/controller/common/imagekit_auth.js
+++ app/controller/common/imagekit_auth.js
@@ -1,7 +1,22 @@
 const jwt = require('jsonwebtoken');
+const uuid = require('uuid');
+const crypto = require("crypto");
+const privateKey = process.env.IMAGEKIT_PRIVATEKEY;
 
 exports.token = (req, res) => {
-  console.log("Generating imagekit token");
+  var token = req.query.token || uuid.v4();
+  var expire = req.query.expire || parseInt(Date.now()/1000)+2400;
+  var privateAPIKey = `${privateKey}`;
+  var signature = crypto.createHmac('sha1', privateAPIKey).update(token+expire).digest('hex');
+  res.status(200);
+  res.send({
+      token : token,
+      expire : expire,
+      signature : signature
+  });
+};
+
+exports.fullToken = (req, res) => {
   const payload = req.body;
   const token = jwt.sign(payload.uploadPayload, "private_6v4LZpKVbg5FWEccdoxrXoxWlyU=", {
     expiresIn: payload.expire,
@@ -11,9 +26,17 @@ exports.token = (req, res) => {
       kid: payload.publicKey,
     },
   });
+
+  var expire = payload.expire || parseInt(Date.now() / 1000) + 2400;
+  var privateAPIKey = "private_6v4LZpKVbg5FWEccdoxrXoxWlyU=";
+  var signature = crypto.createHmac('sha1', privateAPIKey).update(token + expire).digest('hex');
   res.set({
     "Access-Control-Allow-Origin": "*"
   })
   res.status(200);
-  res.send({ token });
-};
+  res.send({
+    token: token,
+    expire: expire,
+    signature: signature
+  })
+}
--- app/route/common/imagekit_auth.js
+++ app/route/common/imagekit_auth.js
@@ -2,5 +2,5 @@ module.exports = (app) => {
     const controller = require('../../controller/common/imagekit_auth.js');
     const path = process.env.CONTEXT_PATH + '/imagekit-token';
     // Public routes
-    app.post(path, controller.token);
+    app.get(path, controller.token);
 }

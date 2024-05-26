8df0aa47a88cf37c8a8a96e232f01cef57d7f4cb
Navaneetha Krishnan <nava.arul@gmail.com>
2024-05-26 08:31:12 +0100

    Add ImageKit API

--- app/controller/common/imagekit.js
+++ app/controller/common/imagekit.js
@@ -3,8 +3,57 @@ const uuid = require("uuid");
 const crypto = require("crypto");
 const http = require("https");
 const { Buffer } = require("node:buffer");
+var ImageKit = require("imagekit");
 
 const privateKey = process.env.IMAGEKIT_PRIVATEKEY;
+const publicKey = process.env.IMAGEKIT_PUBLICKEY;
+const urlEndpoint = process.env.IMAGEKIT_URL_ENDPOINT;
+
+// Initialize
+var imageKit = new ImageKit({
+    publicKey: `${publicKey}`,
+    privateKey: `${privateKey}`,
+    urlEndpoint: `${urlEndpoint}`,
+});
+
+exports.listFiles = (req, res) => {
+    var options = {};
+    if (req.query.path) {
+        options.path = req.query.path;
+    }
+    console.log("List files " + JSON.stringify(options));
+    imageKit.listFiles(options, function(error, result) {
+        if (error) console.log(error);
+        else {
+            res.send(result);
+        }
+    });
+};
+
+exports.deleteFile = (req, res) => {
+    console.log("Deleting of file " + req.query.fileId);
+    imageKit.deleteFile(req.query.fileId, function(error, result) {
+        if (error) console.log(error);
+        else {
+            res.status(204);
+            res.send();
+        }
+    });
+};
+
+exports.getFile = (req, res) => {
+    console.log("Fetching details of file " + req.query.fileId);
+    imageKit.getFileDetails(req.query.fileId, function(error, result) {
+        if (error) {
+            console.log(error);
+            res.status(400);
+            res.send(error);
+        } else {
+            res.status(200);
+            res.send(result);
+        }
+    });
+};
 
 exports.token = (req, res) => {
     var token = req.query.token || uuid.v4();
@@ -14,85 +63,6 @@ exports.token = (req, res) => {
         .createHmac("sha1", privateAPIKey)
         .update(token + expire)
         .digest("hex");
-    res.status(200);
-    res.send({
-        token: token,
-        expire: expire,
-        signature: signature,
-    });
-};
-
-exports.delete = (req, res) => {
-    var fileId = req.query.fileId;
-    console.log("Deleting image file " + fileId);
-    var privateAPIKey = `${privateKey}`;
-    const buf = Buffer.from(privateAPIKey);
-    const base64EncodedPrivateKey = buf.toString("base64");
-    console.log("Private key base64 " + base64EncodedPrivateKey);
-    // Define the options object
-    const options = {
-        method: "DELETE", // Specify the HTTP method
-        hostname: "api.imagekit.io", // Specify the hostname
-        port: 443, // Specify the port (443 for HTTPS)
-        path: "/v1/files/" + fileId, // Specify the path
-        headers: {
-            'Authorization': base64EncodedPrivateKey + ":"
-        }
-    };
-    // Make the request
-    const request = http.request(options, (response) => {
-        // Handle the response
-        console.log("Status code:", response.statusCode); // Print the status code
-        console.log("Headers:", response.headers); // Print the headers
-
-        // Get the data from the response
-        let data = "";
-        response.on("data", (chunk) => {
-            data += chunk; // Concatenate the chunks
-        });
-
-        // When the response is finished, print the data
-        response.on("end", () => {
-            console.error("Req:", req.headers);
-            console.log("Data:", data); // Print the data
-        });
-    });
-
-    // Handle any errors
-    request.on("error", (err) => {
-        console.error("Req:", req.headers); // Print the error message
-        console.error("Error:", err.message); // Print the error message
-    });
-
-    // End the request
-    request.end();
-    res.status(204);
-    res.send();
-};
-
-exports.fullToken = (req, res) => {
-    const payload = req.body;
-    const token = jwt.sign(
-        payload.uploadPayload,
-        "private_6v4LZpKVbg5FWEccdoxrXoxWlyU=", {
-            expiresIn: payload.expire,
-            header: {
-                alg: "HS256",
-                typ: "JWT",
-                kid: payload.publicKey,
-            },
-        }
-    );
-
-    var expire = payload.expire || parseInt(Date.now() / 1000) + 2400;
-    var privateAPIKey = "private_6v4LZpKVbg5FWEccdoxrXoxWlyU=";
-    var signature = crypto
-        .createHmac("sha1", privateAPIKey)
-        .update(token + expire)
-        .digest("hex");
-    res.set({
-        "Access-Control-Allow-Origin": "*",
-    });
     res.status(200);
     res.send({
         token: token,
--- app/route/common/imagekit.js
+++ app/route/common/imagekit.js
@@ -1,8 +1,12 @@
 module.exports = (app) => {
     const controller = require('../../controller/common/imagekit.js');
-    const tokenPath = process.env.CONTEXT_PATH + '/imagekit-token';
-    const deleteFilePath = process.env.CONTEXT_PATH + '/imagekit-delete-file';
+    const tokenPath = process.env.CONTEXT_PATH + '/imagekit/token';
+    const deleteFilePath = process.env.CONTEXT_PATH + '/imagekit/file';
+    const getFilePath = process.env.CONTEXT_PATH + '/imagekit/file';
+    const listFiles = process.env.CONTEXT_PATH + '/imagekit/files';
     // Public routes
     app.get(tokenPath, controller.token);
-    app.delete(deleteFilePath, controller.delete);
+    app.delete(deleteFilePath, controller.deleteFile);
+    app.get(getFilePath, controller.getFile);
+    app.get(listFiles, controller.listFiles);
 }
